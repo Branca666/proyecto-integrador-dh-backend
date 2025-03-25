dd7e15a84eacfc0be1142af5a88c2a92e92c95e8
branca666 <ezequielauge@hotmail.com>
2025-03-25 00:35:24 -0300

    Debug test

--- GlobeOnClick/src/main/java/com/backend/globeonclick/controller/TourPackageController.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/controller/TourPackageController.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tourPackages")
+@RequestMapping("/api/v1/tourPackages")
 @RequiredArgsConstructor
 @Tag(name = "Package Controller", description = "Endpoints para gestión de paquetes turísticos")
 public class TourPackageController {

ac375b64aac8aca95c84fc10561c5401b3501098
branca666 <ezequielauge@hotmail.com>
2025-03-25 00:17:38 -0300

    Revert

--- GlobeOnClick/src/main/java/com/backend/globeonclick/controller/TourPackageController.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/controller/TourPackageController.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/tourPackages")
+@RequestMapping("/api/tourPackages")
 @RequiredArgsConstructor
 @Tag(name = "Package Controller", description = "Endpoints para gestión de paquetes turísticos")
 public class TourPackageController {

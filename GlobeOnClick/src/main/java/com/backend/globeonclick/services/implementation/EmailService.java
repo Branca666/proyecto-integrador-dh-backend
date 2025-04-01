46b0a0827e920db19830526025220ed763edd911
branca666 <ezequielauge@hotmail.com>
2025-03-31 23:45:36 -0300

    Cambios para despliegue

--- GlobeOnClick/src/main/java/com/backend/globeonclick/services/implementation/EmailService.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/services/implementation/EmailService.java
@@ -19,7 +19,7 @@ public class EmailService {
     @Value("${resend.api.key}")
     private String resendApiKey;
 
-    private final String baseUrl = "http://localhost:5173";
+    private final String baseUrl = "http://localhost:5173", "${VITE_API_URL}";
     private final String RESEND_API_URL = "https://api.resend.com/emails";
     private final RestTemplate restTemplate = new RestTemplate();
 
